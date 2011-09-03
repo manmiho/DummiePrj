252a9e7edf7dfbc64d69d9544f251ba6b854f223
Pongsakorn Noksapa <manmiho@hotmail.com>
2011-09-03 10:25:32 +0700

    First Commit
    Window Based Template

---
+++ DummiePrj/DummiePrjAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  DummiePrjAppDelegate.h
+//  DummiePrj
+//
+//  Created by Pongsakorn Noksapa on 9/3/11.
+//  Copyright 2011 RSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DummiePrjAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ DummiePrj/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DummiePrj
+//
+//  Created by Pongsakorn Noksapa on 9/3/11.
+//  Copyright 2011 RSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
